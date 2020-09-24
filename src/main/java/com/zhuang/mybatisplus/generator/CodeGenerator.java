87c9fb1a3d4dd40ebb36b09096d9c120887ebea3
weibin268 <binweizhuang@hotmail.com>
2020-09-24 23:15:58 +0800

    aa

--- src/main/java/com/zhuang/mybatisplus/generator/CodeGenerator.java
+++ src/main/java/com/zhuang/mybatisplus/generator/CodeGenerator.java
@@ -17,36 +17,80 @@ import java.util.Scanner;
 // 演示例子，执行 main 方法控制台输入模块表名回车自动生成对应项目目录中
 public class CodeGenerator {
 
-    public void generate(){
-        // 代码生成器
-        AutoGenerator mpg = new AutoGenerator();
+    private AutoGenerator autoGenerator;
 
+
+    public CodeGenerator setDsDriverName(String driverName) {
+        autoGenerator.getDataSource().setDriverName(driverName);
+        return this;
+    }
+
+    public CodeGenerator setDsUrl(String url) {
+        autoGenerator.getDataSource().setUrl(url);
+        return this;
+    }
+
+    public CodeGenerator setDsUsername(String username) {
+        autoGenerator.getDataSource().setUsername(username);
+        return this;
+    }
+
+    public CodeGenerator setDsPassword(String password) {
+        autoGenerator.getDataSource().setUsername(password);
+        return this;
+    }
+
+    public CodeGenerator setAuthorName(String authorName) {
+        autoGenerator.getGlobalConfig().setAuthor(authorName);
+        return this;
+    }
+
+    public CodeGenerator setModuleName(String moduleName) {
+        autoGenerator.getPackageInfo().setModuleName(moduleName);
+        return this;
+    }
+
+    public CodeGenerator setBasePackage(String basePackage) {
+        autoGenerator.getPackageInfo().setParent(basePackage);
+        return this;
+    }
+
+    public CodeGenerator setTableNames(String tableNames) {
+        autoGenerator.getStrategy().setInclude(tableNames.split(","));
+        return this;
+    }
+
+    public CodeGenerator() {
+        this(System.getProperty("user.dir"));
+    }
+
+    public CodeGenerator(String projectPath) {
+        // 代码生成器
+        autoGenerator = new AutoGenerator();
         // 全局配置
-        GlobalConfig gc = new GlobalConfig();
-        String projectPath = System.getProperty("user.dir");
-        gc.setOutputDir(projectPath + "/src/main/java");
-        gc.setAuthor("jobob");
-        gc.setOpen(false);
-        // gc.setSwagger2(true); 实体属性 Swagger2 注解
-        mpg.setGlobalConfig(gc);
+        GlobalConfig globalConfig = new GlobalConfig();
+        globalConfig.setOutputDir(projectPath + "/src/main/java");
+        globalConfig.setAuthor("zwb");
+        globalConfig.setOpen(false);
+        //globalConfig.setSwagger2(true); 实体属性 Swagger2 注解
+        autoGenerator.setGlobalConfig(globalConfig);
 
         // 数据源配置
-        DataSourceConfig dsc = new DataSourceConfig();
-        dsc.setUrl("jdbc:mysql://weibin268.top:3306/cloud_system?useUnicode=true&useSSL=false&characterEncoding=utf8");
-        // dsc.setSchemaName("public");
-        dsc.setDriverName("com.mysql.jdbc.Driver");
-        dsc.setUsername("root");
-        dsc.setPassword("123456");
-        mpg.setDataSource(dsc);
+        DataSourceConfig dataSourceConfig = new DataSourceConfig();
+        dataSourceConfig.setDriverName("com.mysql.jdbc.Driver");
+        dataSourceConfig.setUrl("jdbc:mysql://weibin268.top:3306/cloud_system?useUnicode=true&useSSL=false&characterEncoding=utf8");
+        dataSourceConfig.setUsername("root");
+        dataSourceConfig.setPassword("123456");
+        autoGenerator.setDataSource(dataSourceConfig);
 
         // 包配置
-        PackageConfig pc = new PackageConfig();
-        pc.setModuleName(scanner("模块名"));
-        pc.setParent("com.baomidou.ant");
-        mpg.setPackageInfo(pc);
+        PackageConfig packageConfig = new PackageConfig();
+        packageConfig.setModuleName("module1");
+        packageConfig.setParent("com.zhuang.test");
+        autoGenerator.setPackageInfo(packageConfig);
 
         // 自定义配置
-        InjectionConfig cfg = new InjectionConfig() {
+        InjectionConfig injectionConfig = new InjectionConfig() {
             @Override
             public void initMap() {
                 // to do nothing
@@ -59,45 +103,23 @@ public class CodeGenerator {
         // String templatePath = "/templates/mapper.xml.vm";
 
         // 自定义输出配置
-        List<FileOutConfig> focList = new ArrayList<>();
+        List<FileOutConfig> fileOutConfigList = new ArrayList<>();
         // 自定义配置会被优先输出
-        focList.add(new FileOutConfig(templatePath) {
+        fileOutConfigList.add(new FileOutConfig(templatePath) {
             @Override
             public String outputFile(TableInfo tableInfo) {
                 // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
-                return projectPath + "/src/main/resources/mapper/" + pc.getModuleName()
+                return projectPath + "/src/main/resources/mapper/" + packageConfig.getModuleName()
                         + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
             }
         });
-        /*
-        cfg.setFileCreate(new IFileCreate() {
-            @Override
-            public boolean isCreate(ConfigBuilder configBuilder, FileType fileType, String filePath) {
-                // 判断自定义文件夹是否需要创建
-                checkDir("调用默认方法创建的目录，自定义目录用");
-                if (fileType == FileType.MAPPER) {
-                    // 已经生成 mapper 文件判断存在，不想重新生成返回 false
-                    return !new File(filePath).exists();
-                }
-                // 允许生成模板文件
-                return true;
-            }
-        });
-        */
-        cfg.setFileOutConfigList(focList);
-        mpg.setCfg(cfg);
+        injectionConfig.setFileOutConfigList(fileOutConfigList);
+        autoGenerator.setCfg(injectionConfig);
 
         // 配置模板
         TemplateConfig templateConfig = new TemplateConfig();
-
-        // 配置自定义输出模板
-        //指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
-        // templateConfig.setEntity("templates/entity2.java");
-        // templateConfig.setService();
-        // templateConfig.setController();
-
         templateConfig.setXml(null);
-        mpg.setTemplate(templateConfig);
+        autoGenerator.setTemplate(templateConfig);
 
         // 策略配置
         StrategyConfig strategy = new StrategyConfig();
@@ -110,31 +132,16 @@ public class CodeGenerator {
         strategy.setSuperControllerClass("你自己的父类控制器,没有就不用设置!");
         // 写于父类中的公共字段
         strategy.setSuperEntityColumns("id");
-        strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
+        strategy.setInclude("table1");
         strategy.setControllerMappingHyphenStyle(true);
-        strategy.setTablePrefix(pc.getModuleName() + "_");
-        mpg.setStrategy(strategy);
-        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
-        mpg.execute();
+        strategy.setTablePrefix(packageConfig.getModuleName() + "_");
+        autoGenerator.setStrategy(strategy);
+        autoGenerator.setTemplateEngine(new FreemarkerTemplateEngine());
     }
 
-    /**
-     * <p>
-     * 读取控制台内容
-     * </p>
-     */
-    public String scanner(String tip) {
-        Scanner scanner = new Scanner(System.in);
-        StringBuilder help = new StringBuilder();
-        help.append("请输入" + tip + "：");
-        System.out.println(help.toString());
-        if (scanner.hasNext()) {
-            String ipt = scanner.next();
-            if (StringUtils.isNotBlank(ipt)) {
-                return ipt;
-            }
-        }
-        throw new MybatisPlusException("请输入正确的" + tip + "！");
+    public void generate() {
+        autoGenerator.execute();
     }
+
 
 }
--- src/test/java/com/zhuang/mybatisplus/generator/CodeGeneratorTest.java
+++ src/test/java/com/zhuang/mybatisplus/generator/CodeGeneratorTest.java
@@ -6,10 +6,16 @@ public class CodeGeneratorTest {
 
     @Test
     public void generate() {
-        new CodeGenerator().generate();
+        new CodeGenerator()
+                .setDsDriverName("com.mysql.jdbc.Driver\"")
+                .setDsUrl("jdbc:mysql://weibin268.top:3306/cloud_system?useUnicode=true&useSSL=false&characterEncoding=utf8")
+                .setDsUsername("root")
+                .setDsPassword("123456")
+                .setAuthorName("zwb")
+                .setModuleName("user")
+                .setBasePackage("com.zwb.test")
+                .setTableNames("sys_user")
+                .generate();
     }
 
-    public static void main(String[] args) {
-        new CodeGenerator().generate();
-    }
 }
