e3e2e381de81d9210c7956e67da7024940758025
庄伟斌 <zhuangweibin@qq.com>
2020-09-25 12:56:10 +0800

    aa

--- src/test/java/com/zhuang/mybatisplus/generator/CodeGeneratorTest.java
+++ src/test/java/com/zhuang/mybatisplus/generator/CodeGeneratorTest.java
@@ -20,4 +20,20 @@ public class CodeGeneratorTest {
                 .generate();
     }
 
+    @Test
+    public void generate2(){
+        new CodeGenerator("d://temp/codes2")
+                .setDsDriverName("com.microsoft.sqlserver.jdbc.SQLServerDriver")
+                .setDsUrl("jdbc:sqlserver://192.168.1.130:1433;DatabaseName=yd_ws_gd_zhaoqing_dev")
+                .setDsUsername("sa")
+                .setDsPassword("123456")
+                .setAuthorName("zwb")
+                .setBasePackage("com.zwb.test")
+                .setModuleName("user")
+                .setTableNames("yd_ws_water_user")
+                .setTablePrefix("yd_ws_")
+                .setSwagger2(true)
+                .generate();
+    }
+
 }
