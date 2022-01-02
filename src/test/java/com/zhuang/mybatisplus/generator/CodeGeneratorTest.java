3b1b536833baeb842e642a8321470fed5bbc495b
zwb <binweizhuang@hotmail.com>
2022-01-02 11:30:09 +0800

    aa

--- src/test/java/com/zhuang/mybatisplus/generator/CodeGeneratorTest.java
+++ src/test/java/com/zhuang/mybatisplus/generator/CodeGeneratorTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class CodeGeneratorTest {
 
     @Test
-    public void generate() {
+    public void mysql() {
         new CodeGenerator("jdbc:mysql://127.0.0.1:3306/upms?useUnicode=true&useSSL=false&characterEncoding=utf8", "root", "123456")
                 .config(config -> {
                     config.setOutputDir("/Users/zhuang/Documents/temp")
@@ -18,4 +18,17 @@ public class CodeGeneratorTest {
                 .generate();
     }
 
+    @Test
+    public void mssql() {
+        new CodeGenerator("jdbc:sqlserver://192.168.0.200:1433;database=upms", "sa", "a@123456")
+                .config(config -> {
+                    config.setOutputDir("/Users/zhuang/Documents/temp")
+                            .setAuthor("zwb")
+                            .setBasePackage("com.zhuang.test")
+                            .setModuleName("test")
+                            .setTableNames("test")
+                            .setSwagger(true);
+                })
+                .generate();
+    }
 }
