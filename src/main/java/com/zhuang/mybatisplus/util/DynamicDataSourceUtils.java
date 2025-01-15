27efdd77f874a25415fa3723a66bf88a9cebbb4a
庄伟斌 <zhuangweibin@qq.com>
2025-01-15 17:43:06 +0800

    aa

--- src/main/java/com/zhuang/mybatisplus/util/DynamicDataSourceUtils.java
+++ src/main/java/com/zhuang/mybatisplus/util/DynamicDataSourceUtils.java
@@ -1,17 +1,46 @@
 package com.zhuang.mybatisplus.util;
 
+import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
 import com.baomidou.dynamic.datasource.toolkit.DynamicDataSourceContextHolder;
+import org.apache.ibatis.mapping.DatabaseIdProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.sql.SQLException;
 import java.util.function.Supplier;
 
+@Component
 public class DynamicDataSourceUtils {
 
-    public static  <T> T execute(String ds, Supplier<T> func) {
+    private static DynamicDataSourceUtils _this;
+    @Autowired
+    private DynamicRoutingDataSource dynamicRoutingDataSource;
+    @Autowired
+    private DatabaseIdProvider databaseIdProvider;
+
+    @PostConstruct
+    private void init() {
+        _this = this;
+    }
+
+    public static <T> T execute(String ds, Supplier<T> func) {
         DynamicDataSourceContextHolder.push(ds);
         try {
             return func.get();
         } finally {
             DynamicDataSourceContextHolder.poll();
+        }
+    }
+
+    public static String getDataBaseId() {
+        String currentDataSource = DynamicDataSourceContextHolder.peek();
+        DataSource dataSource = _this.dynamicRoutingDataSource.getDataSource(currentDataSource);
+        try {
+            return _this.databaseIdProvider.getDatabaseId(dataSource);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
